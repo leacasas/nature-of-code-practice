f16dc27f3113f88e9117abc57866bec9ae426ea8
Leandro Gabriel Casas <leandrogabriel.casas@gmail.com>
2017-03-24 11:08:24 -0300

    Flow Fields 1

---
+++ flow fields/flowfield.js
@@ -0,0 +1,59 @@
+// jshint ignore: start
+var FlowField = function(r){
+    this.density = r;
+    this.cols = width / this.density;
+    this.rows = height / this.density;
+    this.field = createArray(this.cols, this.rows);
+    this.init();
+};
+FlowField.prototype.init = function(){
+    // Reseed noise to get new field every time.
+    noiseSeed(int(random(10000)));
+    var xoff = 0;
+    for(var i = 0; i < this.cols; i++){
+        var yoff = 0;
+        for(var j = 0; j < this.rows; j++){
+            var theta = map(noise(xoff, yoff), 0, 1, 0, TWO_PI);
+            this.field[i][j] = createVector(cos(theta), sin(theta));
+            yoff += 0.1;
+        }
+        xoff += 0.1;
+    }
+};
+FlowField.prototype.display = function(){
+    for (var i = 0; i < this.cols; i++) {
+      for (var j = 0; j < this.rows; j++) {
+        drawVector(this.field[i][j], i * this.density, j * this.density, this.density - 2);
+      }
+    }
+
+    function drawVector(vector, x, y, scale){
+        push();
+        var arrowSize = 4;
+        translate(x, y);
+        stroke(100, 100, 200, 80);
+        rotate(vector.heading());
+        var len = vector.mag() * scale;
+        line(0, 0, len, 0);
+        line(len, 0, len - arrowSize, arrowSize / 2);
+        line(len, 0, len - arrowSize, arrowSize / -2);
+        pop();
+    }
+};
+FlowField.prototype.lookup = function(position){
+    var column = int(constrain(position.x / this.density, 0, this.cols - 1));
+    var row = int(constrain(position.y / this.density, 0, this.rows - 1));
+    return this.field[column][row].copy();
+};
+
+// Aux
+function createArray(length) {
+    var arr = new Array(length || 0), i = length;
+
+    if (arguments.length > 1) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        while(i--) arr[length - 1 - i] = createArray.apply(this, args);
+    }
+
+    return arr;
+}
---
+++ flow fields/vehicle.js
@@ -0,0 +1,52 @@
+// jshint ignore: start
+var Vehicle = function(p, maxSpeed, maxForce){
+    this.position = p.copy();
+    this.velocity = createVector(0, 0);
+    this.acceleration = createVector(0, 0);
+    this.maxSpeed = maxSpeed;
+    this.maxForce = maxForce;
+    this.r = 4 + maxSpeed * 2;
+};
+// Calculates steering force towards target.
+// Steering force = desired velocity - current velocity
+Vehicle.prototype.follow = function(flowField){ 
+    var desired = flowField.lookup(this.position);
+    desired.mult(this.maxSpeed);
+    var steer = p5.Vector.sub(desired, this.velocity);
+    steer.limit(this.maxForce);
+    this.applyForce(steer);
+};
+// We could add mass. Accel = Force / Mass
+Vehicle.prototype.applyForce = function(force){
+    this.acceleration.add(force); 
+};
+Vehicle.prototype.update = function(){
+    this.velocity.add(this.acceleration);
+    this.velocity.limit(this.maxSpeed);
+    this.position.add(this.velocity);
+    this.acceleration.mult(0);
+};
+Vehicle.prototype.borders = function(){
+    if (this.position.x < -this.r) 
+        this.position.x = width + this.r;
+    if (this.position.y < -this.r) 
+        this.position.y = height + this.r;
+    if (this.position.x > width + this.r) 
+        this.position.x = -this.r;
+    if (this.position.y > height + this.r) 
+        this.position.y = -this.r;
+};
+Vehicle.prototype.draw = function(){
+    var theta = this.velocity.heading() + PI/2;
+    fill(0, 255, 0);
+    noStroke()
+    push();
+    translate(this.position.x, this.position.y);
+    rotate(theta);
+    beginShape();
+    vertex(0, -this.r * 1.5);
+    vertex(-this.r, this.r);
+    vertex(this.r, this.r);
+    endShape(CLOSE);
+    pop();
+};

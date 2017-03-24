47b25c0d976fa99bd71a860530373b64a59332e1
Leandro Gabriel Casas <leandrogabriel.casas@gmail.com>
2017-03-24 12:06:50 -0300

    Flow fields that change direction

---
+++ flow fields 2/script.js
@@ -0,0 +1,28 @@
+// jshint ignore: start
+var amountOfVehicles = 5;
+var flowField;
+var vehicles = [];
+var debug = true;
+
+function setup(){
+    createCanvas(1040, 640);
+
+    flowField = new FlowField(40);
+
+    for(var i = 0; i < amountOfVehicles; i++)
+        vehicles[i] = new Vehicle(createVector(random(width), random(height)), random(2, 5), random(0.1, 0.5));
+}
+
+function draw(){
+    background(0);
+    
+    flowField.update();
+    if(debug)flowField.display();
+    
+    for(var i = 0; i < amountOfVehicles; i++){
+        vehicles[i].follow(flowField);
+        vehicles[i].update();
+        vehicles[i].borders();
+        vehicles[i].draw();
+    }
+}

6ab15fd16fec14144409c3ec5674d1bf5b8c12c4
Leandro Casas <leandro.casas@globant.com>
2017-05-23 16:36:53 -0300

    Restoring config to some interesting state.

--- Configurable DOL System/config.js
+++ Configurable DOL System/config.js
@@ -1,18 +1,18 @@
 var config = {
-    axiom : "F+F+F+F",
+    axiom : "F-F-F-F",
     generations : 4,
     reduction : 0.25,
     turtle : {
         startX : 330,
         startY : 330,
-        omega : "F+F+F+F", // Should be the same as axiom above.
+        omega : "F-F-F-F", // Should be the same as axiom above.
         delta : 90,
-        d : 185,
+        d : 400,
     },
     productions : [
         {
             predecesor : "F",
-            succesor : "FF-F-F-F-F-F+F"
+            succesor : "FF-F-F-F-FF"
         }
     ]
 };

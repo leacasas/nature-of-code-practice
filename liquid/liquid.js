8ca6cb52f9dbce8353ef89da12ea4caf893a5e3f
Leandro Gabriel Casas <leandrogabriel.casas@gmail.com>
2020-04-19 11:18:55 -0300

    latest changes.

--- liquid/liquid.js
+++ liquid/liquid.js
@@ -38,62 +38,64 @@ function reset(){
     }
 }
 
-var Liquid = function(x, y, w, h, c){
-    this.x = x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
-    this.c = c;
-};
-Liquid.prototype.contains = function(mover){
-    var mp = mover.position;
-    return mp.x > this.x 
-        && mp.x < this.x + this.w 
-        && mp.y > this.y 
-        && mp.y < this.y + this.h;
-};
-Liquid.prototype.drag = function(mover){
-    var speed = mover.velocity.mag();
-    var dragMagnitude = this.c * speed * speed;
-
-    var dragForce = mover.velocity.copy();
-    dragForce.mult(-1);
-    dragForce.normalize();
-    dragForce.mult(dragMagnitude);
-
-    return dragForce;
-};
-Liquid.prototype.display = function(){
-    noStroke();
-    fill(0, 170, 255);
-    rect(this.x, this.y, this.w, this.h);
-};
-
-var Mover = function(m, x, y){
-    this.mass = m;
-    this.size = m * 16;
-    this.position = createVector(x, y);
-    this.velocity = createVector(0, 0);
-    this.acceleration = createVector(0, 0);
-};
-Mover.prototype.applyForce = function(force){
-    var f = p5.Vector.div(force, this.mass);
-    this.acceleration.add(f);
-};
-Mover.prototype.update = function(){
-    this.velocity.add(this.acceleration);
-    this.position.add(this.velocity);
-    this.acceleration.mult(0);
-};
-Mover.prototype.display = function(){
-    stroke(0);
-    strokeWeight(2);
-    fill(127, 200);
-    ellipse(this.position.x, this.position.y, this.size, this.size);
-};
-Mover.prototype.checkEdges = function(){
-    if(this.position.y + this.size / 2 > height){
-        this.velocity.y *= -0.9;
-        this.position.y = height - this.size / 2;
+class Liquid {
+    constructor(x, y, w, h, c) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.c = c;
     }
-};
+    contains(mover) {
+        var mp = mover.position;
+        return mp.x > this.x
+            && mp.x < this.x + this.w
+            && mp.y > this.y
+            && mp.y < this.y + this.h;
+    }
+    drag(mover) {
+        var speed = mover.velocity.mag();
+        var dragMagnitude = this.c * speed * speed;
+        var dragForce = mover.velocity.copy();
+        dragForce.mult(-1);
+        dragForce.normalize();
+        dragForce.mult(dragMagnitude);
+        return dragForce;
+    }
+    display() {
+        noStroke();
+        fill(0, 170, 255);
+        rect(this.x, this.y, this.w, this.h);
+    }
+}
+
+class Mover {
+    constructor(m, x, y) {
+        this.mass = m;
+        this.size = m * 16;
+        this.position = createVector(x, y);
+        this.velocity = createVector(0, 0);
+        this.acceleration = createVector(0, 0);
+    }
+    applyForce(force) {
+        var f = p5.Vector.div(force, this.mass);
+        this.acceleration.add(f);
+    }
+    update() {
+        this.velocity.add(this.acceleration);
+        this.position.add(this.velocity);
+        this.acceleration.mult(0);
+    }
+    display() {
+        stroke(0);
+        strokeWeight(2);
+        fill(127, 200);
+        ellipse(this.position.x, this.position.y, this.size, this.size);
+    }
+    checkEdges() {
+        if (this.position.y + this.size / 2 > height) {
+            this.velocity.y *= -0.9;
+            this.position.y = height - this.size / 2;
+        }
+    }
+}
